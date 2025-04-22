ef009bacb4716ec9363bf81b3ef2d59f6fd16c03
Gustavo Tironi <b51083@fgv.edu.br>
2025-04-21 23:08:07 -0300

    Pipeline completa e funcionando

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 // ConcreteExtractor.h
 #include "framework/BaseClasses.h"
+#include "framework/Transformer.h"
 #include "framework/Dataframe.h"
 #include "framework/Manager.h"
-#include "framework/Transformer.h"
 #include <thread>
 #include <chrono>
 #include <iostream>
 #include <numeric>
-
-std::vector<Buffer<Dataframe>*> make_input_vector(Buffer<Dataframe>& buffer) {
-    return { &buffer };
-}
 
 // Função utilitária no topo do arquivo
 template<typename... Buffers>
@@ -25,6 +21,12 @@ string division(string str1, string str2){
         return "Erro: Divisão por zero";
     }
     return to_string(num1 / num2);
+}
+
+string multiplication(string str1, string str2){
+    double num1 = stod(str1);
+    double num2 = stod(str2);
+    return to_string(num1 * num2);
 }
 
 class filter_hotel : public Transformer<Dataframe> {
@@ -63,33 +65,30 @@ class join: public Transformer<Dataframe> {
         }
 };
 
-class ocupacao_pesquisa: public Transformer<Dataframe> {
+class disponibilidade: public Transformer<Dataframe> {
     public:
         using Transformer::Transformer; // Herda o construtor
 
         Dataframe run(std::vector<Dataframe*> input) override {
 
-            input[0]->bColumnOperation("preco_sum", "count", division, "preco_dividido");
-            // cout << *input[0] << endl;
+            input[0]->bColumnOperation("data_ida_dia_count", "quantidade_pessoas_count", division, "disponibilidade");
             return *input[0];
-        }        
+        }
 };
 
-class groupby_voo : public Transformer<Dataframe> {
+
+class faturamento: public Transformer<Dataframe> {
     public:
         using Transformer::Transformer; // Herda o construtor
 
         Dataframe run(std::vector<Dataframe*> input) override {
 
-            std::vector<std::string> vstrColumnsToAggregate = {"assentos_ocupados", "assentos_totais"};
+            input[0]->bColumnOperation("preco_sum", "quantidade_pessoas_count", division, "preco_medio");
 
-            std::vector<std::string> vstrColumnsToGroup = {"cidade_destino"};
+            input[0]->bColumnOperation("preco_medio", "data_ida_dia_count", multiplication, "faturamento_esperado");
 
-            Dataframe df_gruped = input[0]-> dfGroupby(vstrColumnsToGroup, vstrColumnsToAggregate, true, false, false);
-
-            //std::cout << df_gruped;
-
-            return df_gruped;
+            std::cout << *input[0];
+            return *input[0];
         }
 };
 
@@ -113,6 +112,7 @@ class taxa_ocup_voo: public Transformer<Dataframe> {
         }
 };
 
+
 class DataPrinter : public Loader<Dataframe> {
     public:
         using Loader::Loader; // Inherit constructor
@@ -122,7 +122,7 @@ class DataPrinter : public Loader<Dataframe> {
 
         void run(Dataframe df) override {
             if (!headerPrinted) {
-                // df.printHeader(std::cout, df); // Print the dataframe head
+                //df.printHeader(std::cout, df); // Print the dataframe head
                 headerPrinted = true;
             }
 
@@ -131,89 +131,94 @@ class DataPrinter : public Loader<Dataframe> {
         }
     };
 
+
+
 int main() {
-    std::vector<int> paralelismos = {1, 3, 5, 7, 9, 11};
+    // Inicializa o Manager
+    Manager<Dataframe> manager(7);
 
-    for (int p : paralelismos) {
-        std::vector<long long> tempos_execucao;
+    // Pipeline Hoteis e Pesquisas ------------------------------------------------------------------------
+    Extrator<Dataframe> extrator_pesquisa("./mock/data/dados_viagens_2025.csv", "csv", 100);
+    manager.addExtractor(&extrator_pesquisa);
 
-        for (int i = 0; i < 10; ++i) {
-            // Inicializa o Manager
-            Manager<Dataframe> manager(p);
+    Extrator<Dataframe> extrator_hoteis("./mock/data/dados_hoteis_2025.csv", "csv", 1000);
+    manager.addExtractor(&extrator_hoteis);
 
-            // Cria os extratores
-            Extrator<Dataframe> extrator_pesquisa("./mock/data/dados_viagens_2025.csv", "csv", 1000);
-            manager.addExtractor(&extrator_pesquisa);
+    filter_hotel filtroocupacao(make_input_vector(extrator_hoteis.get_output_buffer()));
+    manager.addTransformer(&filtroocupacao);
 
-            Extrator<Dataframe> extrator_hoteis("./mock/data/dados_hoteis2_2025.csv", "csv", 50);
-            manager.addExtractor(&extrator_hoteis);
+    std::vector<std::string> vstrColumnsToAggregate = {"quantidade_pessoas", "preco"};
+    std::vector<string> group = {"cidade_destino"};
+    std::vector<string> ops = {"sum"};
 
-            std::vector<std::string> vstrColumnsToAggregate = {"quantidade_pessoas", "preco"};
-            std::vector<string> group = {"cidade_destino"};
-            std::vector<string> ops = {"sum"};
+    GroupByTransformer<Dataframe> groupby_hotel(make_input_vector(filtroocupacao.get_output_buffer()),
+                                                group,
+                                                vstrColumnsToAggregate,
+                                                ops);
+    manager.addTransformer(&groupby_hotel);
 
-            GroupByTransformer<Dataframe> groupby_hotel(make_input_vector(extrator_hoteis.get_output_buffer()),
-                                                        group,
-                                                        vstrColumnsToAggregate,
-                                                        ops);
-            manager.addTransformer(&groupby_hotel);
+    vstrColumnsToAggregate = {"data_ida_dia", "data_ida_mes", "data_ida_ano"};
+    group = {"cidade_destino"};
+    ops = {"sum"};
 
-            vstrColumnsToAggregate = {"data_ida_dia", "data_ida_mes", "data_ida_ano"};
-            group = {"cidade_destino"};
-            ops = {"sum"};
+    GroupByTransformer<Dataframe> groupby_pesquisa(make_input_vector(extrator_pesquisa.get_output_buffer()),
+                                                    group,
+                                                    vstrColumnsToAggregate,
+                                                    ops);
+    manager.addTransformer(&groupby_pesquisa);
 
-            GroupByTransformer<Dataframe> groupby_pesquisa(make_input_vector(extrator_pesquisa.get_output_buffer()),
-                                                        group,
-                                                        vstrColumnsToAggregate,
-                                                        ops);
-            manager.addTransformer(&groupby_pesquisa);
+    std::vector<Buffer<Dataframe>*> inputs_buffers;
+    inputs_buffers.push_back(&groupby_hotel.get_output_buffer());
+    inputs_buffers.push_back(&groupby_pesquisa.get_output_buffer());
 
-            std::vector<Buffer<Dataframe>*> inputs_buffers;
-            inputs_buffers.push_back(&groupby_hotel.get_output_buffer());
-            inputs_buffers.push_back(&groupby_pesquisa.get_output_buffer());
+    join join_transformer(inputs_buffers, 2);
+    manager.addTransformer(&join_transformer);
 
-            join join_transformer(inputs_buffers, 1);
-            manager.addTransformer(&join_transformer);
+    disponibilidade disp(make_input_vector(join_transformer.get_output_buffer()));
+    manager.addTransformer(&disp);
 
-            ocupacao_pesquisa ocup_1(make_input_vector(join_transformer.get_output_buffer()));
-            manager.addTransformer(&ocup_1);
+    faturamento fatur(make_input_vector(join_transformer.get_output_buffer()));
+    manager.addTransformer(&fatur);
 
-            DataPrinter loader2(ocup_1.get_output_buffer());
-            manager.addLoader(&loader2);
+    DataPrinter loader1(disp.get_output_buffer());
+    manager.addLoader(&loader1);
 
-            Extrator<Dataframe> extrator_voos("./mock/data/dados_voos_2025.csv", "csv", 1000);
-            manager.addExtractor(&extrator_voos);
+    DataPrinter loader2(fatur.get_output_buffer());
+    manager.addLoader(&loader2);
 
-            vstrColumnsToAggregate = {"quantidade_pessoas", "preco"};
-            group = {"cidade_destino"};
-            ops = {"sum", "sum"}; // uma operação por coluna
+    //Pipeline Voos ------------------------------------------------------------------------
+    Extrator<Dataframe> extrator_voos("./mock/data/dados_voos_2025.csv", "csv", 1000);
+    manager.addExtractor(&extrator_voos);
 
-            GroupByTransformer<Dataframe> groupbyvoo(
-                make_input_vector(extrator_hoteis.get_output_buffer()),
-                group,
-                vstrColumnsToAggregate,
-                ops
-            );
+    vstrColumnsToAggregate = {"assentos_ocupados", "assentos_totais"};
+    group = {"cidade_destino"};
+    ops = {"sum"};
 
-            manager.addTransformer(&groupby_hotel);
+    GroupByTransformer<Dataframe> groupbyvoo(
+        make_input_vector(extrator_voos.get_output_buffer()),
+        group,
+        vstrColumnsToAggregate,
+        ops
+    );
 
-            taxa_ocup_voo taxaocupvoo(make_input_vector(groupbyvoo.get_output_buffer()));
-            manager.addTransformer(&taxaocupvoo);
+    manager.addTransformer(&groupbyvoo);
 
-            DataPrinter loader1(taxaocupvoo.get_output_buffer());
-            manager.addLoader(&loader1);
+    taxa_ocup_voo taxaocupvoo(make_input_vector(groupbyvoo.get_output_buffer()));
+    manager.addTransformer(&taxaocupvoo);
 
-            auto start_time = std::chrono::high_resolution_clock::now();
-            manager.run();
-            auto end_time = std::chrono::high_resolution_clock::now();
+    DataPrinter loader3(taxaocupvoo.get_output_buffer());
+    manager.addLoader(&loader3);
 
-            long long duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-            tempos_execucao.push_back(duration);
-        }
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-        double media = std::accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0.0) / tempos_execucao.size();
-        std::cout << "Paralelismo: " << p << " -> Tempo médio: " << media << " ms" << std::endl;
-    }
+    // Start the pipeline
+    manager.run();
 
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    std::cout << "Tempo de execução: " << duration << " ms" << std::endl;
+
+    // The manager's destructor will clean up everything
     return 0;
 }
